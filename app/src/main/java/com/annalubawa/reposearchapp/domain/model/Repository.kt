b144864987f6c027f00a94d91e689f74f1aa0bdf
anffie <36477661+anffie@users.noreply.github.com>
2020-12-13 14:24:28 +0100

    Added safe args to RepositoryDetailsFragment

--- app/src/main/java/com/annalubawa/reposearchapp/domain/model/Repository.kt
+++ app/src/main/java/com/annalubawa/reposearchapp/domain/model/Repository.kt
@@ -1,8 +1,12 @@
 package com.annalubawa.reposearchapp.domain.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Repository(
     val name: String,
     val numberOfStars: Int,
     val repoUrl: String,
     val owner: RepositoryOwner
-)
+): Parcelable
--- app/src/main/java/com/annalubawa/reposearchapp/domain/model/RepositoryOwner.kt
+++ app/src/main/java/com/annalubawa/reposearchapp/domain/model/RepositoryOwner.kt
@@ -1,6 +1,10 @@
 package com.annalubawa.reposearchapp.domain.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class RepositoryOwner(
     val avatarUrl: String,
     val name: String
-)
+): Parcelable
