3ac92e905c92000c83b010f96e2a4cc1ea19726d
anffie <36477661+anffie@users.noreply.github.com>
2020-12-13 11:56:44 +0100

    Layouts updated, added data binding, recycler adapter

---
+++ app/src/main/java/com/annalubawa/reposearchapp/ui/adapters/BindingAdapters.kt
@@ -0,0 +1,18 @@
+package com.annalubawa.reposearchapp.ui.adapters
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+@BindingAdapter("imageBinding")
+fun loadImage(view: ImageView, url: String) {
+    Glide.with(view)
+        .load(url)
+        .into(view)
+}
+
+@BindingAdapter("integerTextBinding")
+fun loadInteger(view: TextView, number: Int) {
+    view.text = "$number"
+}
---
+++ app/src/main/java/com/annalubawa/reposearchapp/ui/adapters/RepositoriesRecyclerViewAdapter.kt
@@ -0,0 +1,34 @@
+package com.annalubawa.reposearchapp.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.annalubawa.reposearchapp.R
+import com.annalubawa.reposearchapp.databinding.ListItemLayoutBinding
+import com.annalubawa.reposearchapp.domain.model.Repository
+
+class RepositoriesRecyclerViewAdapter (
+    private val items: List<Repository>,
+    private val onItemSelected: (item: Repository) -> Unit
+): RecyclerView.Adapter<RepositoriesRecyclerViewAdapter.RepositoriesViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        RepositoriesViewHolder(
+            DataBindingUtil.inflate<ListItemLayoutBinding>(
+                LayoutInflater.from(parent.context),
+                R.layout.list_item_layout,
+                parent,
+                false
+            )
+        )
+
+    override fun onBindViewHolder(holder: RepositoriesViewHolder, position: Int) {
+        holder.repoContainerBinding.repo = items[position]
+    }
+
+    override fun getItemCount(): Int  = items.size
+
+    inner class RepositoriesViewHolder(val repoContainerBinding: ListItemLayoutBinding)
+        : RecyclerView.ViewHolder(repoContainerBinding.root)
+}
